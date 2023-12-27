bac7e22d3e3ac0644c2cb9235ac34e70d110e497
Mohjamoutawadii <mohjamoutawadii1@gmail.com>
2023-12-27 11:45:53 +0100

    first commit

---
+++ app/src/main/java/com/example/darssa/config/APIConfig.java
@@ -0,0 +1,9 @@
+package com.example.darssa.config;
+
+public class APIConfig {
+    public static final String BASE_URL = "http://128.10.6.77:5050";
+
+    public static String getRegisterUrl() {
+        return BASE_URL;
+    }
+}
